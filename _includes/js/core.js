7f50eb9a9300847dc0a2d8bbe5a4d62754448263
Constant V <constantvallee@gmail.com>
2020-11-28 12:10:56 -0500

    Fix FOUC dark theme

--- _includes/js/core.js
+++ _includes/js/core.js
@@ -1,31 +1,4 @@
-(function() {
-  // window.__onThemeChange = function() {}
-  function setTheme(newTheme) {
-    document.documentElement.setAttribute('class', newTheme)
-    // window.__theme = newTheme
-    // preferredTheme = newTheme
-    // window.__onThemeChange(newTheme)
-  }
-
-  var preferredTheme = localStorage.getItem('theme')
-  // try {
-  //   preferredTheme = localStorage.getItem('theme')
-  // } catch (err) {}
-
-  window.__setPreferredTheme = function(newTheme) {
-    setTheme(newTheme)
-    try {
-      localStorage.setItem('theme', newTheme)
-    } catch (err) {}
-  }
-
-  // var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
-  // darkQuery.addListener(function(e) {
-  //   window.__setPreferredTheme(e.matches ? 'dark' : 'light')
-  // })
-
-  setTheme(preferredTheme )
-
+(() => {
   const toggleBtn = document.querySelector('.toggle-theme');
   
   toggleBtn.addEventListener('click', toggleTheme);
