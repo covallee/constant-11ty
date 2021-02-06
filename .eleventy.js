be8d1afb7b35c76e34187160aa52970acd836526
Constant V <constantvallee@gmail.com>
2021-02-06 18:39:24 -0500

    feat: switch to 11ty-image

--- .eleventy.js
+++ .eleventy.js
@@ -1,45 +1,63 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require('luxon')
+const fs = require('fs')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginNavigation = require('@11ty/eleventy-navigation')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
 
+const Image = require('@11ty/eleventy-img')
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    formats: ['avif', 'jpeg'],
+  })
 
-  eleventyConfig.setDataDeepMerge(true);
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async',
+  }
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  return Image.generateHTML(metadata, imageAttributes)
+}
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginSyntaxHighlight)
+  eleventyConfig.addPlugin(pluginNavigation)
 
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy-LL-dd");
-  });
-  
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
+  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+  })
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+  })
+
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n)
     }
-    return array.slice(0, n);
-  });
+    return array.slice(0, n)
+  })
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'))
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
-  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
 
-  eleventyConfig.addPassthroughCopy("css/fonts");
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('favicon.ico')
+  eleventyConfig.addPassthroughCopy('favicon-16x16.png')
+  eleventyConfig.addPassthroughCopy('favicon-32x32.png')
+  eleventyConfig.addPassthroughCopy('apple-touch-icon.png')
+
+  eleventyConfig.addPassthroughCopy('css/fonts')
 
   let markdownLibrary = markdownIt({
     html: true,
@@ -48,8 +66,8 @@ module.exports = function(eleventyConfig) {
   }).use(markdownItAnchor, {
     permalink: true,
     permalinkBefore: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+  })
+  eleventyConfig.setLibrary('md', markdownLibrary)
 }
