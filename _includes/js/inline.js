13517fd1aad2b0396ccf0bd090e084d786dea9a4
Constant V <constantvallee@gmail.com>
2021-02-20 08:23:41 -0500

    fix: bug of theme btn

--- _includes/js/inline.js
+++ _includes/js/inline.js
@@ -1,28 +1,28 @@
-(function() {
-  window.__onThemeChange = function() {}
+(function () {
+  window.__onThemeChange = function () {};
   function setTheme(newTheme) {
-    document.documentElement.setAttribute('class', newTheme)
-    window.__theme = newTheme
-    preferredTheme = newTheme
-    window.__onThemeChange(newTheme)
+    document.documentElement.setAttribute('class', newTheme);
+    window.__theme = newTheme;
+    preferredTheme = newTheme;
+    window.__onThemeChange(newTheme);
   }
 
-  var preferredTheme = localStorage.getItem('theme')
+  var preferredTheme = localStorage.getItem('theme');
   try {
-    preferredTheme = localStorage.getItem('theme')
+    preferredTheme = localStorage.getItem('theme');
   } catch (err) {}
 
-  window.__setPreferredTheme = function(newTheme) {
-    setTheme(newTheme)
+  window.__setPreferredTheme = function (newTheme) {
+    setTheme(newTheme);
     try {
-      localStorage.setItem('theme', newTheme)
+      localStorage.setItem('theme', newTheme);
     } catch (err) {}
-  }
+  };
 
-  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
-  darkQuery.addListener(function(e) {
-    window.__setPreferredTheme(e.matches ? 'dark' : 'light')
-  })
+  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  darkQuery.addListener(function (e) {
+    window.__setPreferredTheme(e.matches ? 'dark' : 'light');
+  });
 
-  setTheme(preferredTheme )
+  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
 })();
